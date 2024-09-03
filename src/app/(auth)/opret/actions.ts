00856fe222b888f8915a8cd99b8cb7a579dcf692
anvidev <andreasgylche@gmail.com>
2024-09-03 15:03:20 +0200

    creating default location when creating a customer

--- src/app/(auth)/opret/actions.ts
+++ src/app/(auth)/opret/actions.ts
@@ -7,6 +7,7 @@ import { ActionError } from "@/lib/safe-action/error"
 import { emailService } from "@/service/email"
 import { EmailWelcomeCustomer } from "@/components/email/email-welcome-customer"
 import { userService } from "@/service/user"
+import { locationService } from "@/service/location"
 
 export const createCustomerAction = publicAction
   .schema(createCustomerValidation)
@@ -24,6 +25,14 @@ export const createCustomerAction = publicAction
     const newCustomer = await customerService.create(parsedInput)
     if (!newCustomer) {
       throw new ActionError("Der gik noget galt med at oprette dig som kunde")
+    }
+
+    const newLocation = await locationService.create({
+      customerID: newCustomer.id,
+      name: "Hovedlokation"
+    })
+    if (!newLocation) {
+      throw new ActionError("Der gik noget galt med at oprette en lokation")
     }
 
     const activationLink = await customerService.createActivationLink({ customerID: newCustomer.id, email: newCustomer.email })
