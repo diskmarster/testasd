db2be38eefd3786aaba2d60dbd23538ce3e403f8
anvidev <andreasgylche@gmail.com>
2024-09-03 15:02:39 +0200

    added location table definition

--- src/lib/database/schema/customer.ts
+++ src/lib/database/schema/customer.ts
@@ -1,6 +1,7 @@
 import { Plan } from "@/data/customer.types";
 import { sql } from "drizzle-orm";
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text, primaryKey, PrimaryKey } from "drizzle-orm/sqlite-core";
+import { userTable } from "./auth";
 
 export const customerTable = sqliteTable("nl_customer", {
   id: integer("id").notNull().primaryKey({ autoIncrement: true }),
@@ -28,3 +29,28 @@ export type NewCustomerLink = typeof customerLinkTable.$inferInsert
 export type CustomerLink = typeof customerLinkTable.$inferSelect
 export type CustomerLinkID = CustomerLink['id']
 export type PartialCustomerLink = Partial<CustomerLink>
+
+export const locationsTable = sqliteTable('nl_locations', {
+  id: integer("id").notNull().primaryKey({ autoIncrement: true }),
+  customerID: integer('customer_id').notNull().references(() => customerTable.id, { onDelete: 'cascade' }),
+  name: text('name').notNull()
+})
+
+export type NewLocation = typeof locationsTable.$inferInsert
+export type Location = typeof locationsTable.$inferSelect
+export type LocationID = Location['id']
+export type PartialLocation = Partial<Location>
+
+export const linkLocationToUserTable = sqliteTable('nl_link_location_to_user', {
+  locationID: integer('location_id').notNull().references(() => locationsTable.id, { onDelete: 'cascade' }),
+  userID: integer('user_id').notNull().references(() => userTable.id),
+  isPrimary: integer('is_primary', { mode: 'boolean' }).notNull().default(false)
+}, (table) => {
+  return {
+    pk: primaryKey({ columns: [table.userID, table.locationID] })
+  }
+})
+
+export type NewLinkLocationToUser = typeof linkLocationToUserTable.$inferInsert
+export type LinkLocationToUser = typeof linkLocationToUserTable.$inferSelect
+export type LinkLocationToUserPK = { userID: LinkLocationToUser['userID'], locationID: LinkLocationToUser['locationID'] }
