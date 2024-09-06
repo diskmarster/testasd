ebbb0a51e3b964a67e78c77018fb9be62566359a
Tobias <tobias@skancode.dk>
2024-09-06 13:58:20 +0200

    add helper function for signing and verifying JWTs

--- src/service/user.utils.ts
+++ src/service/user.utils.ts
@@ -1,3 +1,4 @@
+import * as jwt from 'jsonwebtoken'
 import { User, UserNoHash } from '@/lib/database/schema/auth'
 import { hash, verify } from '@node-rs/argon2'
 const MEMORY_COST = 19456
@@ -29,4 +30,55 @@ export function userDTO(u: User): UserNoHash {
   const { hash, ...rest } = u
   const user = { ...rest }
   return user
+}
+
+export type JWTObject = {
+  sessionId: string,
+  user: UserNoHash,
+}
+
+export function signJwt(payload: JWTObject): string {
+  return jwt.sign(payload, process.env.JWT_SECRET as string)
+}
+
+type JWTFailed = {
+  name: 'TokenExpiredError' | 'JsonWebTokenError' | 'NotBeforeError',
+  message: string,
+  expiredAt?: number,
+  date?: string,
+}
+
+export type VerifyJWTResponse = {
+  ok: true,
+  data: JWTObject,
+  error?: undefined,
+} | {
+  ok: false,
+  data?: undefined,
+  error: JWTFailed,
+}
+export function verifyJWT(jwtString: string): VerifyJWTResponse {
+  try {
+  const payload = jwt.verify(jwtString, process.env.JWT_SECRET as string)
+
+  let jwtObj: JWTObject
+  if (typeof payload == "string") {
+    jwtObj = JSON.parse(payload);
+  } else {
+    jwtObj = {
+      sessionId: payload.sessionId,
+      user: payload.user,
+    }
+  }
+
+  return {
+    ok: true,
+    data: jwtObj,
+  }
+  } catch (e) {
+    return {
+      ok: false,
+      error: e as JWTFailed,
+    }
+  }
 }
