b59ccf91a0fcda342a86b78e08323462baf493f8
anvidev <andreasgylche@gmail.com>
2024-09-06 14:02:38 +0200

    added if not exists to existing mig files

--- src/lib/database/migrations/0000_puzzling_justin_hammer.sql
+++ src/lib/database/migrations/0000_puzzling_justin_hammer.sql
@@ -1,11 +1,11 @@
-CREATE TABLE `nl_session` (
+CREATE TABLE IF NOT EXISTS `nl_session` (
 	`id` text PRIMARY KEY NOT NULL,
 	`user_id` integer NOT NULL,
 	`expires_at` integer NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `nl_user`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-CREATE TABLE `nl_user` (
+CREATE TABLE IF NOT EXISTS `nl_user` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`name` text NOT NULL,
 	`email` text NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE `nl_user` (
 	FOREIGN KEY (`customer_id`) REFERENCES `nl_customer`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-CREATE TABLE `nl_customer_link` (
+CREATE TABLE IF NOT EXISTS `nl_customer_link` (
 	`id` text PRIMARY KEY NOT NULL,
 	`customer_id` integer NOT NULL,
 	`email` text NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE `nl_customer_link` (
 	FOREIGN KEY (`location_id`) REFERENCES `nl_location`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-CREATE TABLE `nl_customer` (
+CREATE TABLE IF NOT EXISTS `nl_customer` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`plan` text NOT NULL,
 	`company` text NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE `nl_customer` (
 	`updated` integer DEFAULT (unixepoch()) NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE `nl_link_location_to_user` (
+CREATE TABLE IF NOT EXISTS `nl_link_location_to_user` (
 	`location_id` text NOT NULL,
 	`user_id` integer NOT NULL,
 	`customer_id` integer NOT NULL,
@@ -50,12 +50,12 @@ CREATE TABLE `nl_link_location_to_user` (
 	FOREIGN KEY (`customer_id`) REFERENCES `nl_customer`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-CREATE TABLE `nl_location` (
+CREATE TABLE IF NOT EXISTS `nl_location` (
 	`id` text PRIMARY KEY NOT NULL,
 	`customer_id` integer NOT NULL,
 	`name` text NOT NULL,
 	FOREIGN KEY (`customer_id`) REFERENCES `nl_customer`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-CREATE UNIQUE INDEX `nl_user_email_unique` ON `nl_user` (`email`);--> statement-breakpoint
-CREATE UNIQUE INDEX `nl_customer_email_unique` ON `nl_customer` (`email`);
+CREATE UNIQUE INDEX IF NOT EXISTS `nl_user_email_unique` ON `nl_user` (`email`);--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS `nl_customer_email_unique` ON `nl_customer` (`email`);
