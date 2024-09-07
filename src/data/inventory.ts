6b5c0e5f45afa79f9256ebb01d31705369ebac69
anvi <andreasgylche@gmail.com>
2024-09-07 14:29:05 +0200

    query function for getting an inventory for a location

--- src/data/inventory.ts
+++ src/data/inventory.ts
@@ -1,13 +1,34 @@
 import { db, TRX } from "@/lib/database"
 import { LocationID } from "@/lib/database/schema/customer"
-import { inventoryTable } from "@/lib/database/schema/inventory"
-import { eq } from "drizzle-orm"
+import { batchTable, inventoryTable, placementTable, productTable, unitTable } from "@/lib/database/schema/inventory"
+import { eq, getTableColumns } from "drizzle-orm"
+import { FormattedInventory } from "./inventory.types"
+
+const PRODUCT_COLS = getTableColumns(productTable)
+const PLACEMENT_COLS = getTableColumns(placementTable)
+const BATCH_COLS = getTableColumns(batchTable)
+const UNIT_COLS = getTableColumns(unitTable)
 
 export const inventory = {
-  getInventoryByLocationID: async function(locationID: LocationID, trx: TRX = db) {
-    const todo = await db
-      .select()
+  getInventoryByLocationID: async function(locationID: LocationID, trx: TRX = db): Promise<FormattedInventory[]> {
+    const inventory: FormattedInventory[] = await trx
+      .select({
+        inserted: inventoryTable.inserted,
+        updated: inventoryTable.updated,
+        quantity: inventoryTable.quantity,
+        customerID: inventoryTable.customerID,
+        locationID: inventoryTable.locationID,
+        product: { ...PRODUCT_COLS, unit: UNIT_COLS.name },
+        placement: { ...PLACEMENT_COLS },
+        batch: { ...BATCH_COLS }
+      })
       .from(inventoryTable)
       .where(eq(inventoryTable.locationID, locationID))
+      .innerJoin(productTable, eq(productTable.id, inventoryTable.productID))
+      .innerJoin(placementTable, eq(placementTable.id, inventoryTable.placementID))
+      .innerJoin(batchTable, eq(batchTable.id, inventoryTable.batchID))
+      .innerJoin(unitTable, eq(unitTable.id, productTable.unitID))
+
+    return inventory
   }
 }
--- src/data/inventory.types.ts
+++ src/data/inventory.types.ts
@@ -1,5 +1,18 @@
+import { CustomerID, LocationID } from "@/lib/database/schema/customer";
+import { Batch, Placement, Product } from "@/lib/database/schema/inventory";
 import { z } from "zod";
 
 export const historyTypeZodSchema = z.enum(['tilgang', 'afgang', 'regulering']);
 export type HistoryType = z.infer<typeof historyTypeZodSchema>;
 export const historyTypes = historyTypeZodSchema.options as readonly HistoryType[];
+
+export type FormattedInventory = {
+  inserted: Date
+  updated: Date
+  quantity: number
+  customerID: CustomerID
+  locationID: LocationID
+  product: Product & { unit: string }
+  placement: Placement
+  batch: Batch
+}
