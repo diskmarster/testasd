77ee10d9efc1389b4ffcb200a37916cccca6f44c
anvi <andreasgylche@gmail.com>
2024-09-07 14:30:37 +0200

    fetching inventory and displaying data as json (wip)

--- src/app/(site)/oversigt/page.tsx
+++ src/app/(site)/oversigt/page.tsx
@@ -1,14 +1,22 @@
+import { signOutAction } from "@/app/(auth)/log-ud/actions";
 import { SiteWrapper } from "@/components/common/site-wrapper";
+import { inventoryService } from "@/service/inventory";
 import { locationService } from "@/service/location";
 import { sessionService } from "@/service/session";
 
 export default async function Home() {
-  const { user } = await sessionService.validate()
-  const location = locationService.getLastVisited(user?.id!)
+  const { session, user } = await sessionService.validate()
+  if (!session) return signOutAction()
+
+  const location = await locationService.getLastVisited(user.id!)
+  if (!location) return null // TODO: make some error page
+
+  const inventory = await inventoryService.getInventory(location)
+
   return (
-    <SiteWrapper title="Nem Lager Boilerplate">
-      <pre>Hello, {JSON.stringify(user, null, 2)}</pre>
+    <SiteWrapper title="Oversigt">
       <p>Sidst besøgt lokation: {location}</p>
+      <pre>{JSON.stringify(inventory, null, 2)}</pre>
     </SiteWrapper>
   );
 }
