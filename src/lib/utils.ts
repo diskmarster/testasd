52ced3b55dcbd186d4aa461a20d0f3402bee2217
anvi <andreasgylche@gmail.com>
2024-09-08 14:45:10 +0200

    added date and currency util functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(date: Date) {
+  return new Intl.DateTimeFormat('en-GB', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZone: 'CET',
+  }).format(date)
+}
+
+const dkCurrencyFormat = new Intl.NumberFormat("da-DK", {
+  style: 'currency',
+  currency: 'DKK',
+})
+
+export function numberToDKCurrency(val: number): string {
+  return dkCurrencyFormat.format(val)
+}
