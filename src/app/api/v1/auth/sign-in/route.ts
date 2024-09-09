b98df795d64697c95b1b586cf35db60900c0bb01
Tobias <tobias@skancode.dk>
2024-09-09 10:17:46 +0200

    user sessionService instead of creating the session manually

--- src/app/api/v1/auth/sign-in/route.ts
+++ src/app/api/v1/auth/sign-in/route.ts
@@ -1,4 +1,5 @@
 import { lucia } from "@/lib/lucia";
+import { sessionService } from "@/service/session";
 import { userService } from "@/service/user";
 import { signJwt } from "@/service/user.utils";
 import { NextRequest, NextResponse } from "next/server";
@@ -12,52 +13,63 @@ const signInSchema = z.object({
 })
 
 export async function POST(request: NextRequest): Promise<NextResponse<unknown>> {
-	if (request.headers.get("content-type") != "application/json") {
+	try {
+		if (request.headers.get("content-type") != "application/json") {
+			return NextResponse.json({
+				msg: "Request body skal være json format",
+			}, {
+				status: 400,
+			})
+		}
+
+		const body = await request.json()
+
+		const zodRes = signInSchema.safeParse(body)
+
+		if (!zodRes.success) {
+			return NextResponse.json({
+				msg: "Indlæsning af data fejlede",
+				errorMessages: zodRes.error.flatten().formErrors,
+				error: zodRes.error,
+			}, {
+				status: 400,
+			})
+		}
+
+		const { email, password } = zodRes.data
+
+		const user = await userService.verifyPassword(email, password)
+
+		if (user == undefined) {
+			return NextResponse.json({
+				msg: "Ugyldig email og password",
+			}, {
+				status: 401
+			})
+		}
+
+		const sessionId = await sessionService.create(user.id)
+
+		const jwt = signJwt({
+			sessionId: sessionId,
+			user: user,
+		})
+
 		return NextResponse.json({
-			msg: "Request body skal være json format",
+			msg: "Log ind fuldendt",
+			jwt,
+			user,
 		}, {
-			status: 400,
+			status: 201,
+		})
+	} catch (e) {
+		console.error(e)
+
+		return NextResponse.json({
+			msg: "Der skete en fejl på serveren, prøv igen.",
+			error: e
+		}, {
+			status: 500
 		})
 	}
-
-	const body = await request.json()
-
-	const zodRes = signInSchema.safeParse(body)
-
-	if (!zodRes.success) {
-		return NextResponse.json({
-			msg: "Indlæsning af data fejlede",
-			errorMessages: zodRes.error.flatten().formErrors,
-			error: zodRes.error,
-		}, {
-			status: 400,
-		})
-	}
-
-	const { email, password } = zodRes.data
-
-	const user = await userService.verifyPassword(email, password)
-
-	if (user == undefined) {
-		return NextResponse.json({
-			msg: "Ugyldig email og password",
-		}, {
-			status: 400
-		})
-	}
-
-	const session = await lucia.createSession(user.id, user)
-
-	const jwt = signJwt({
-		sessionId: session.id,
-		user: user,
-	})
-
-	return NextResponse.json({
-		msg: "Log ind fuldendt",
-		jwt,
-		user,
-	}, {
-		status: 201,
-	})
 }
