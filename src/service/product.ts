865dbfca0b0bd1f09e79a7b2bcfd8ee057b16bf1
Tobias <tobias@skancode.dk>
2024-09-10 10:06:11 +0200

    make getAllProducts in product service async

--- src/service/product.ts
+++ src/service/product.ts
@@ -3,12 +3,7 @@ import { CustomerID } from "@/lib/database/schema/customer";
 import { Product } from "@/lib/database/schema/inventory";
 
 export const productService = {
-  getAllProducts: (customerID: CustomerID): Promise<Product[]> | undefined => {
-    try {
-      return product.getAllProducts(customerID)
-    } catch(e) {
-      console.error("Error getting products from database", JSON.stringify(e, null, 2))
-      Promise.reject(`Error getting products from database ${JSON.stringify(e, null, 2)}`)
-    }
+  getAllProducts: async (customerID: CustomerID): Promise<Product[]> => {
+    return await product.getAllProducts(customerID)
   }
 }
