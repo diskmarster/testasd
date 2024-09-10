dea510732ccd54daa93e949da92920b0b4f452f0
anvi <andreasgylche@gmail.com>
2024-09-10 20:14:29 +0200

    minor change

--- src/components/common/site-wrapper.tsx
+++ src/components/common/site-wrapper.tsx
@@ -22,7 +22,7 @@ export function SiteWrapper({
         !fullWidth && 'container',
         !noPadding && 'gap-4 pt-4',
       )}>
-      <div className='flex w-full space-y-0.5'>
+      <div className='flex w-full'>
         <div className='flex-1'>
           {title && (
             <h1 className='whitespace-balance max-w-xl text-xl font-semibold leading-tight tracking-tighter md:text-2xl'>
