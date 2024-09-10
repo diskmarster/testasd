b1647a12c87e9b6848eba5c52ad4a74f118eada1
anvi <andreasgylche@gmail.com>
2024-09-10 20:17:23 +0200

    added gap to screen wrapper actions

--- src/components/common/site-wrapper.tsx
+++ src/components/common/site-wrapper.tsx
@@ -33,7 +33,7 @@ export function SiteWrapper({
             <p className='text-muted-foreground'>{description}</p>
           )}
         </div>
-        {actions && <div>{actions}</div>}
+        {actions && <div className='flex items-center gap-2'>{actions}</div>}
       </div>
       {children}
     </div>
