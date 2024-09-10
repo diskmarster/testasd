7206332007d96f013754ac0cd7d0d01dc94dd80a
Tobias <tobias@skancode.dk>
2024-09-10 09:51:36 +0200

    try catch for getAllProducts on productService

--- src/service/product.ts
+++ src/service/product.ts
@@ -3,7 +3,12 @@ import { CustomerID } from "@/lib/database/schema/customer";
 import { Product } from "@/lib/database/schema/inventory";
 
 export const productService = {
-  getAllProducts: (customerID: CustomerID): Promise<Product[]> => {
-    return product.getAllProducts(customerID)
+  getAllProducts: (customerID: CustomerID): Promise<Product[]> | undefined => {
+    try {
+      return product.getAllProducts(customerID)
+    } catch(e) {
+      console.error("Error getting products from database", JSON.stringify(e, null, 2))
+      Promise.reject(`Error getting products from database ${JSON.stringify(e, null, 2)}`)
+    }
   }
 }
