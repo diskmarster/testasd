6fb59286cab7993804d66f980fd45c04f3766d83
Lujhna <jakob_h_95@hotmail.com>
2024-09-11 15:18:59 +0200

    Extended validation a bit + .superRefine

--- src/app/(site)/produkter/opret/validation.ts
+++ src/app/(site)/produkter/opret/validation.ts
@@ -1,14 +1,32 @@
 import { z } from 'zod'
 
-export const createProductValidation = z.object({
-  customerID: z.number(),
-  groupID: z.number(),
-  unitID: z.number().min(1, 'Skal være udfyldt.'),
-  text1: z.string().min(1, 'Skal være udfyldt.'),
-  text2: z.string(),
-  text3: z.string(),
-  sku: z.string().min(1, 'Skal være udfyldt.'),
-  barcode: z.string().min(1, 'Skal være udfyldt.'),
-  costPrice: z.coerce.number(),
-  salesPrice: z.coerce.number(),
-})
+export const createProductValidation = z
+  .object({
+    customerID: z.coerce.number(),
+    groupID: z.coerce.number(),
+    unitID: z.coerce.number(),
+    text1: z.string().min(1, 'Skal være udfyldt.'),
+    text2: z.string(),
+    text3: z.string(),
+    sku: z.string().min(1, 'Skal være udfyldt.'),
+    barcode: z.string().min(1, 'Skal være udfyldt.'),
+    costPrice: z.coerce.number(),
+    salesPrice: z.coerce.number(),
+  })
+  .superRefine((val, ctx) => {
+    if (val.costPrice < 0) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['costPrice'],
+        message: 'Kostpris kan ikke være negativ',
+      })
+    }
+
+    if (val.costPrice == undefined) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['costPrice'],
+        message: 'Kostpris skal udfyldes',
+      })
+    }
+  })
