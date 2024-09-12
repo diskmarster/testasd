09bd2d0face0395134a09353002641165f6a1cc0
Lujhna <jakob_h_95@hotmail.com>
2024-09-12 13:26:00 +0200

    Added more precise error messaging for create products form

--- src/service/products.ts
+++ src/service/products.ts
@@ -1,12 +1,25 @@
 import { product } from '@/data/products'
 import { NewProduct, Product } from '@/lib/database/schema/inventory'
+import { ActionError } from '@/lib/safe-action/error'
+import { LibsqlError } from '@libsql/client'
 
 export const productService = {
   create: async function (
     productData: NewProduct,
   ): Promise<Product | undefined> {
-    const newProduct = await product.create(productData)
-    if (!newProduct) return undefined
-    return newProduct
+    try {
+      const newProduct = await product.create(productData)
+      if (!newProduct) return undefined
+      return newProduct
+    } catch (err) {
+      if (err instanceof LibsqlError) {
+        if (err.message.includes('barcode')) {
+          throw new ActionError('Stregkoden findes allerede')
+        }
+        if (err.message.includes('sku')){
+          throw new ActionError('Varenr. findes allerede')
+        }
+      }
+    }
   },
 }
