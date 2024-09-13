436f313e7e6b5c08772c17e741e182f425d07dc4
Lujhna <jakob_h_95@hotmail.com>
2024-09-13 14:30:31 +0200

    Table-overview, columns and refactoring in form-create-product

---
+++ src/components/products/table-overview.tsx
@@ -0,0 +1,178 @@
+'use client'
+
+import { getProductOverviewColumns } from '@/app/(site)/produkter/columns'
+import { TableGroupedCell } from '@/components/table/table-grouped-cell'
+import { TablePagination } from '@/components/table/table-pagination'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { Plan } from '@/data/customer.types'
+import { Product } from '@/lib/database/schema/inventory'
+import {
+  ColumnFiltersState,
+  ExpandedState,
+  flexRender,
+  getCoreRowModel,
+  getExpandedRowModel,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFilteredRowModel,
+  getGroupedRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  GroupingState,
+  RowSelectionState,
+  SortingState,
+  Updater,
+  useReactTable,
+  VisibilityState,
+} from '@tanstack/react-table'
+import { User } from 'lucia'
+import { useEffect, useMemo, useState } from 'react'
+
+const ROW_SELECTION_ENABLED = true
+const COLUMN_FILTERS_ENABLED = true
+const ROW_PER_PAGE = [100, 250, 500, 1000]
+interface Props {
+  data: Product[]
+  plan: Plan
+  user: User
+}
+
+export function ProductOverview({ data, plan, user }: Props) {
+  const LOCALSTORAGE_KEY = 'product_cols'
+  const columns = useMemo(() => getProductOverviewColumns(plan), [plan])
+
+  const [sorting, setSorting] = useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+  const [rowSelection, setRowSelection] = useState<RowSelectionState>({})
+  const [grouping, setGrouping] = useState<GroupingState>(['sku'])
+  const [expanded, setExpanded] = useState<ExpandedState>({})
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
+
+  useEffect(() => {
+    const visibility = JSON.parse(
+      localStorage.getItem(LOCALSTORAGE_KEY) || '{}',
+    )
+    setColumnVisibility(visibility)
+  }, [LOCALSTORAGE_KEY])
+
+  const handleVisibilityChange = (updaterOrValue: Updater<VisibilityState>) => {
+    if (LOCALSTORAGE_KEY) {
+      if (typeof updaterOrValue === 'function') {
+        const currentState = JSON.parse(
+          localStorage.getItem(LOCALSTORAGE_KEY) || '{}',
+        )
+
+        const updatedState = updaterOrValue(currentState)
+        localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(updatedState))
+        setColumnVisibility(updatedState)
+      } else {
+        localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(updaterOrValue))
+
+        setColumnVisibility(updaterOrValue)
+      }
+    } else {
+      setColumnVisibility(updaterOrValue)
+    }
+  }
+
+  const table = useReactTable({
+    data,
+    columns,
+
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+    getGroupedRowModel: getGroupedRowModel(),
+    getExpandedRowModel: getExpandedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+
+    groupedColumnMode: 'reorder',
+
+    onColumnFiltersChange: setColumnFilters,
+    onRowSelectionChange: setRowSelection,
+    onSortingChange: setSorting,
+    onGroupingChange: setGrouping,
+    onExpandedChange: setExpanded,
+    onColumnVisibilityChange: handleVisibilityChange,
+
+    enableColumnFilters: COLUMN_FILTERS_ENABLED,
+    enableRowSelection: ROW_SELECTION_ENABLED,
+
+    autoResetExpanded: false,
+    filterFromLeafRows: false,
+
+    state: {
+      columnFilters,
+      rowSelection,
+      sorting,
+      grouping,
+      expanded,
+      columnVisibility,
+    },
+
+    meta: {
+      user,
+    },
+  })
+
+  /* const filterFields = useMemo(() => getProductTableOverviewFilters(products), [])
+   */
+  return (
+    <div>
+      {/* <TableToolbar
+        table={table}
+        options={{ showExport: true, showHideShow: true }}
+        filterFields={filterFields}
+      /> */}
+      <div className='rounded-md border'>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map(headerGroup => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map(header => (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows && table.getRowModel().rows.length > 0 ? (
+              table.getRowModel().rows.map(row => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}>
+                  <TableGroupedCell row={row} />
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className='h-24 text-center'>
+                  Ingen beholdning
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <TablePagination table={table} pageSizes={ROW_PER_PAGE} />
+    </div>
+  )
+}
