acd5212726569ca689590a8ae1e431013c7a32f8
Tobias <tobias@skancode.dk>
2024-09-16 11:09:17 +0200

    actually create regulation in db regulation endpoint

--- src/app/api/v1/regulations/route.ts
+++ src/app/api/v1/regulations/route.ts
@@ -1,10 +1,14 @@
 import { historyTypeZodSchema } from '@/data/inventory.types'
+import { inventoryService } from '@/service/inventory'
 import { validateRequest } from '@/service/user.utils'
 import { NextRequest, NextResponse } from 'next/server'
 import { z } from 'zod'
 
 const createRegulationSchema = z.object({
+	locationId: z.string(),
 	productId: z.coerce.number(),
+	placementId: z.coerce.number().or(z.string()).optional(),
+	batchId: z.coerce.number().or(z.string()).optional(),
 	type: historyTypeZodSchema,
 	quantity: z.coerce.number(),
 })
@@ -55,6 +59,143 @@ export async function POST(
 		const { data } = zodRes
 
 		console.log('data received:', JSON.stringify(data, null, 2))
+
+		let placementId: number
+		if (typeof data.placementId == 'string') {
+			// Create new placement or fetch default for location
+			if (data.placementId == '') {
+				// fetch default
+				const placements = await inventoryService.getPlacementsByID(
+					data.locationId,
+				)
+				const defaultPlacement = placements.find(p => p.name == '-')
+				if (defaultPlacement == undefined) {
+					return NextResponse.json(
+						{
+							msg: 'Kunne ikke finde standard placering for denne lokation, opret en ny placering',
+						},
+						{
+							status: 500,
+						},
+					)
+				}
+				placementId = defaultPlacement.id
+			} else {
+				const res = await inventoryService.createPlacement({
+					locationID: data.locationId,
+					name: data.placementId,
+				})
+				if (res == undefined) {
+					return NextResponse.json(
+						{
+							msg: 'Kunne ikke oprette en ny placering i databasen',
+						},
+						{
+							status: 500,
+						},
+					)
+				}
+				placementId = res.id
+			}
+		} else if (typeof data.placementId == 'number') {
+			placementId = data.placementId
+		} else {
+			// data.placementId should be undefined
+			// Then fetch default placement for location
+			const placements = await inventoryService.getPlacementsByID(
+				data.locationId,
+			)
+			const defaultPlacement = placements.find(p => p.name == '-')
+			if (defaultPlacement == undefined) {
+				return NextResponse.json(
+					{
+						msg: 'Kunne ikke finde standard placering for denne lokation, opret en ny placering',
+					},
+					{
+						status: 500,
+					},
+				)
+			}
+			placementId = defaultPlacement.id
+		}
+
+		let batchId: number
+		if (typeof data.batchId == 'string') {
+			// Create new placement or fetch default for location
+			if (data.batchId == '') {
+				// fetch default
+				const batches = await inventoryService.getBatchesByID(data.locationId)
+				const defaultBatch = batches.find(b => b.batch == '-')
+				if (defaultBatch == undefined) {
+					return NextResponse.json(
+						{
+							msg: 'Kunne ikke finde standard batchnr. for denne lokation, opret et nyt batch nummer',
+						},
+						{
+							status: 500,
+						},
+					)
+				}
+				batchId = defaultBatch.id
+			} else {
+				const res = await inventoryService.createBatch({
+					locationID: data.locationId,
+					batch: data.batchId,
+				})
+				if (res == undefined) {
+					return NextResponse.json(
+						{
+							msg: 'Kunne ikke oprette nyt batch nummer i databasen',
+						},
+						{
+							status: 500,
+						},
+					)
+				}
+				batchId = res.id
+			}
+		} else if (typeof data.batchId == 'number') {
+			batchId = data.batchId
+		} else {
+			// data.batchId should be undefined
+			// Then fetch default placement for location
+			const batches = await inventoryService.getBatchesByID(data.locationId)
+			const defaultBatch = batches.find(b => b.batch == '-')
+			if (defaultBatch == undefined) {
+				return NextResponse.json(
+					{
+						msg: 'Kunne ikke finde standard batchnr. for denne lokation, opret et nyt batch nummer',
+					},
+					{
+						status: 500,
+					},
+				)
+			}
+			batchId = defaultBatch.id
+		}
+
+		if (
+			!(await inventoryService.upsertInventory(
+				'app',
+				user.customerID,
+				user.id,
+				data.locationId,
+				data.productId,
+				placementId,
+				batchId,
+				data.type,
+				data.quantity,
+			))
+		) {
+			return NextResponse.json(
+				{
+					msg: `Kunne ikke oprette en ny ${data.type}, prøv igen`,
+				},
+				{
+					status: 500,
+				},
+			)
+		}
 
 		return NextResponse.json(
 			{
