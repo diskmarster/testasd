d9ae647b46a7e0726145a8bb87093868ced7602c
Lujhna <jakob_h_95@hotmail.com>
2024-09-17 12:10:31 +0200

    added the new type to get the unit and group names to show in the table.

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,11 +1,38 @@
 import { db, TRX } from '@/lib/database'
 import { CustomerID } from '@/lib/database/schema/customer'
 import {
+  groupTable,
   NewProduct,
   Product,
   productTable,
+  unitTable,
 } from '@/lib/database/schema/inventory'
-import { eq } from 'drizzle-orm'
+import { eq, getTableColumns } from 'drizzle-orm'
+import { FormattedProduct } from './products.types'
+
+const UNIT_COLS = getTableColumns(unitTable)
+const GROUP_COLS = getTableColumns(groupTable)
+const PRODUCT_COLS = getTableColumns(productTable)
+
+export const products = {
+  getAllByCustomerID: async function (
+    customerID: CustomerID,
+    trx: TRX = db,
+  ): Promise<FormattedProduct[]> {
+    const products: FormattedProduct[] = await trx
+      .select({
+        ...PRODUCT_COLS,
+        unit: UNIT_COLS.name,
+        group: GROUP_COLS.name,
+      })
+      .from(productTable)
+      .where(eq(productTable.customerID, customerID))
+      .innerJoin(unitTable, eq(unitTable.id, productTable.unitID))
+      .innerJoin(groupTable, eq(groupTable.id, productTable.groupID))
+
+    return products
+  },
+}
 
 export const product = {
   create: async function (
