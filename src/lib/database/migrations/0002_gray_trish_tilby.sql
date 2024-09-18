34c35123e8c1aee67b65510fd34ac1ab921aa154
anvidev <andreasgylche@gmail.com>
2024-09-18 15:22:26 +0200

    new mig files

---
+++ src/lib/database/migrations/0002_gray_trish_tilby.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `nl_reorder` (
+	`location_id` text NOT NULL,
+	`product_id` integer NOT NULL,
+	`customer_id` integer NOT NULL,
+	`minimum` real NOT NULL,
+	`ordered` real DEFAULT 0 NOT NULL,
+	PRIMARY KEY(`product_id`, `location_id`, `customer_id`),
+	FOREIGN KEY (`location_id`) REFERENCES `nl_location`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`product_id`) REFERENCES `nl_product`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`customer_id`) REFERENCES `nl_customer`(`id`) ON UPDATE no action ON DELETE cascade
+);
