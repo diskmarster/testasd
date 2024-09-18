9021124ed109a645fe3aca6d758de6ea075c0b62
anvidev <andreasgylche@gmail.com>
2024-09-18 11:34:15 +0200

    added new icons

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -66,4 +66,42 @@ export const Icons = {
   boxes: Boxes,
   check: Check,
   infinity: Infinity,
+  plusMinus: ({ ...props }) => (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      {...props}>
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M4 7h6' />
+      <path d='M7 4v6' />
+      <path d='M20 18h-6' />
+      <path d='M5 19l14 -14' />
+    </svg>
+  ),
+  replace: ({ ...props }) => (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      {...props}>
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M3 3m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z' />
+      <path d='M15 15m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z' />
+      <path d='M21 11v-3a2 2 0 0 0 -2 -2h-6l3 3m0 -6l-3 3' />
+      <path d='M3 13v3a2 2 0 0 0 2 2h6l-3 -3m0 6l3 -3' />
+    </svg>
+  ),
 }
