bb98a251b7a2771d20b74aeb0283d4e42f19a18a
Lujhna <jakob_h_95@hotmail.com>
2024-09-19 09:30:29 +0200

    Added updateByID to service component. + added more thorough error message.

--- src/service/products.ts
+++ src/service/products.ts
@@ -1,7 +1,8 @@
-import { product} from '@/data/products'
+import { product } from '@/data/products'
 import { FormattedProduct } from '@/data/products.types'
 import { CustomerID } from '@/lib/database/schema/customer'
-import { Inventory, NewProduct, Product } from '@/lib/database/schema/inventory'
+import { Inventory, NewProduct, PartialProduct, Product, ProductID } from '@/lib/database/schema/inventory'
+
 import { ActionError } from '@/lib/safe-action/error'
 import { LibsqlError } from '@libsql/client'
 import { inventoryService } from './inventory'
@@ -70,5 +71,22 @@ export const productService = {
     customerID: CustomerID,
   ): Promise<FormattedProduct[]> {
     return await product.getAllByCustomerID(customerID)
+  },
+
+  updateByID: async function(productID: ProductID, updatedProductData: PartialProduct): Promise<Product | undefined> {
+    try {
+    const updatedProduct = await product.updateByID(productID, updatedProductData)
+    if (!updatedProduct) return undefined
+    return updatedProduct
+  } catch (err) {
+    if (err instanceof LibsqlError) {
+      if (err.message.includes('barcode')) {
+        throw new ActionError('Stregkoden findes allerede')
+      }
+      if (err.message.includes('sku')) {
+        throw new ActionError('Varenr. findes allerede')
+      }
+      }
+    }
   },
 }
