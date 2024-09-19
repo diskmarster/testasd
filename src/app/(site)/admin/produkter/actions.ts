63c1d87630b7aad99d01448d68a4836d30733652
Lujhna <jakob_h_95@hotmail.com>
2024-09-19 09:24:23 +0200

    Added updateProductAction to actions.ts to handle updating products.

--- src/app/(site)/admin/produkter/actions.ts
+++ src/app/(site)/admin/produkter/actions.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { PartialProduct, ProductID } from '@/lib/database/schema/inventory'
 import { adminAction } from '@/lib/safe-action'
 import { ActionError } from '@/lib/safe-action/error'
 import { productService } from '@/service/products'
@@ -13,5 +14,27 @@ export const createProductAction = adminAction
     if (!newProduct) {
       throw new ActionError('Der gik noget galt med at oprette produktet')
     }
-    revalidatePath('/produkter')
+    revalidatePath('/admin/produkter')
   })
+
+export async function updateProductAction(
+  productID: ProductID,
+  updatedProductData: PartialProduct,
+) {
+  try {
+    const updatedProduct = await productService.updateByID(
+      productID,
+      updatedProductData,
+    )
+    if (!updatedProduct) {
+      throw new Error('Failed to update the product')
+    }
+    revalidatePath('/admin/produkter')
+    return { success: true, product: updatedProduct }
+  } catch (err: any) {
+    console.error('Error updating product:', err)
+    return { success: false, serverError: err.message }
+  }
+}
+
+
