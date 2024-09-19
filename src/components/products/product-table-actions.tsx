c3cb14567fa9af36e6960e783a88bfa8a28e8b7c
Lujhna <jakob_h_95@hotmail.com>
2024-09-19 09:27:48 +0200

    Added updateproductsform, props and updatelogic. passed props to updateproductsform.

--- src/components/products/product-table-actions.tsx
+++ src/components/products/product-table-actions.tsx
@@ -1,9 +1,9 @@
-'use client'
-
+import { UpdateProductsForm } from '@/components/products/update-product-form'
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
+import { FormattedProduct } from '@/data/products.types'
 import { Row, Table } from '@tanstack/react-table'
 import { TableActionsWrapper } from '../table/table-actions-wrapper'
-import { FormattedProduct } from '@/data/products.types'
-import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
+import { useState } from 'react'
 
 interface Props {
   table: Table<FormattedProduct>
@@ -11,10 +11,33 @@ interface Props {
 }
 
 export function TableOverviewActions({ table, row }: Props) {
+  const [open, setOpen] = useState<boolean>(false)
+  
+  const handleProductUpdated = () => {
+  }
+
+  // @ts-ignore
+  const units = table.options.meta.units
+  // @ts-ignore
+  const groups = table.options.meta.groups
+
   return (
-    <TableActionsWrapper>
-      <DropdownMenuItem>Redigér</DropdownMenuItem>
-      <DropdownMenuItem>Fjern</DropdownMenuItem>
-    </TableActionsWrapper>
+    <>
+      <TableActionsWrapper>
+        <DropdownMenuItem onClick={() => setOpen(true)} >
+          Redigér produkt
+        </DropdownMenuItem>
+        <DropdownMenuItem>Spær</DropdownMenuItem>
+      </TableActionsWrapper>
+
+      <UpdateProductsForm
+        units={units}
+        groups={groups}
+        productToEdit={row.original}
+        onProductUpdated={handleProductUpdated}
+        isOpen={open}                 
+        setOpen={setOpen}             
+      />
+    </>
   )
 }
