f96dd7fd7bc726a1a6d7323a6f58aa528cd7cf8f
THCSC <tobias@skancode.dk>
2024-09-22 15:43:16 +0200

    add customer data to api sign-in endpoint

--- src/app/api/v1/auth/sign-in/route.ts
+++ src/app/api/v1/auth/sign-in/route.ts
@@ -1,24 +1,32 @@
-import { sessionService } from "@/service/session";
-import { userService } from "@/service/user";
-import { signJwt } from "@/service/user.utils";
-import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
+import { customerService } from '@/service/customer'
+import { sessionService } from '@/service/session'
+import { userService } from '@/service/user'
+import { signJwt } from '@/service/user.utils'
+import { NextRequest, NextResponse } from 'next/server'
+import { z } from 'zod'
 
 const signInSchema = z.object({
 	email: z
-		.string({ message: "Email mangler" })
-		.email({ message: "Email er ugyldig" }),
-	password: z.string({ message: "Password mangler", }).min(8, { message: "Kodeord skal være mindst 8 karakterer." }),
+		.string({ message: 'Email mangler' })
+		.email({ message: 'Email er ugyldig' }),
+	password: z
+		.string({ message: 'Password mangler' })
+		.min(8, { message: 'Kodeord skal være mindst 8 karakterer.' }),
 })
 
-export async function POST(request: NextRequest): Promise<NextResponse<unknown>> {
+export async function POST(
+	request: NextRequest,
+): Promise<NextResponse<unknown>> {
 	try {
-		if (request.headers.get("content-type") != "application/json") {
-			return NextResponse.json({
-				msg: "Request body skal være json format",
-			}, {
-				status: 400,
-			})
+		if (request.headers.get('content-type') != 'application/json') {
+			return NextResponse.json(
+				{
+					msg: 'Request body skal være json format',
+				},
+				{
+					status: 400,
+				},
+			)
 		}
 
 		const body = await request.json()
@@ -26,13 +34,16 @@ export async function POST(request: NextRequest): Promise<NextResponse<unknown>>
 		const zodRes = signInSchema.safeParse(body)
 
 		if (!zodRes.success) {
-			return NextResponse.json({
-				msg: "Indlæsning af data fejlede",
-				errorMessages: zodRes.error.flatten().formErrors,
-				error: zodRes.error,
-			}, {
-				status: 400,
-			})
+			return NextResponse.json(
+				{
+					msg: 'Indlæsning af data fejlede',
+					errorMessages: zodRes.error.flatten().formErrors,
+					error: zodRes.error,
+				},
+				{
+					status: 400,
+				},
+			)
 		}
 
 		const { email, password } = zodRes.data
@@ -40,12 +51,16 @@ export async function POST(request: NextRequest): Promise<NextResponse<unknown>>
 		const user = await userService.verifyPassword(email, password)
 
 		if (user == undefined) {
-			return NextResponse.json({
-				msg: "Ugyldig email og password",
-			}, {
-				status: 401
-			})
+			return NextResponse.json(
+				{
+					msg: 'Ugyldig email og password',
+				},
+				{
+					status: 401,
+				},
+			)
 		}
+		const customer = await customerService.getByID(user.customerID)
 
 		const sessionId = await sessionService.create(user.id)
 
@@ -54,23 +69,30 @@ export async function POST(request: NextRequest): Promise<NextResponse<unknown>>
 			user: user,
 		})
 
-		return NextResponse.json({
-			msg: "Log ind fuldendt",
-			data: {
-				jwt,
-				user,
-			}
-		}, {
-			status: 201,
-		})
+		return NextResponse.json(
+			{
+				msg: 'Log ind fuldendt',
+				data: {
+					jwt,
+					user,
+					customer,
+				},
+			},
+			{
+				status: 201,
+			},
+		)
 	} catch (e) {
 		console.error(e)
 
-		return NextResponse.json({
-			msg: "Der skete en fejl på serveren, prøv igen.",
-			error: e
-		}, {
-			status: 500
-		})
+		return NextResponse.json(
+			{
+				msg: 'Der skete en fejl på serveren, prøv igen.',
+				error: e,
+			},
+			{
+				status: 500,
+			},
+		)
 	}
 }
