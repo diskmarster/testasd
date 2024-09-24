ccef703949fbba7de8d8c5829af4fcb5110eb944
Lujhna <jakob_h_95@hotmail.com>
2024-09-24 10:52:41 +0200

    added pin to signup validation. (also prettier)

--- src/app/(auth)/registrer/[linkID]/validation.ts
+++ src/app/(auth)/registrer/[linkID]/validation.ts
@@ -1,12 +1,15 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 export const signUpValidation = z.object({
   linkID: z.string(),
   clientID: z.coerce.number(),
-  name: z.string().min(2, { message: "Navn skal minimum være 2 karakterer" }),
+  name: z.string().min(2, { message: 'Navn skal minimum være 2 karakterer' }),
   email: z.string().email({ message: 'Email skal være gyldig' }),
-  password: z.string().min(8, { message: 'Kodeord skal minimum være 8 karakterer' }),
-  confirmPassword: z.string().min(8, { message: 'Kodeord skal minimum være 8 karakterer' }),
-  pin: z.string().length(4, { message: "PIN-koden skal være på 4 tal."}),
-  confirmPin: z.string().length(4, { message: "PIN-koden skal være på 4 tal."}),
+  password: z
+    .string()
+    .min(8, { message: 'Kodeord skal minimum være 8 karakterer' }),
+  confirmPassword: z
+    .string()
+    .min(8, { message: 'Kodeord skal minimum være 8 karakterer' }),
+  pin: z.string().length(4, { message: 'PIN-koden skal være på 4 tal.' }),
 })
