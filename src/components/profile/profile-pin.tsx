08fbe452ec0e3e7ad4807e640269811199a0c941
Lujhna <jakob_h_95@hotmail.com>
2024-09-25 12:25:53 +0200

    Sidste refractor

--- src/components/profile/profile-pin.tsx
+++ src/components/profile/profile-pin.tsx
@@ -122,7 +122,7 @@ export function PinDialog() {
                     return
                   }
                   toast(siteConfig.successTitle, {
-                    description: 'PIN blev opdateret',
+                    description: 'PIN-koden blev opdateret',
                   })
                   setOpen(false)
                 })
