75a1e6922aaeb2808f84d3655c7590ffff698e05
Lujhna <jakob_h_95@hotmail.com>
2024-09-27 12:57:08 +0200

    prettier + refractor

--- src/components/inventory/table-units-actions.tsx
+++ src/components/inventory/table-units-actions.tsx
@@ -1,46 +1,43 @@
+import { toggleBarredUnitAction } from '@/app/(site)/sys/enheder/actions'
+import { ModalUpdateUnit } from '@/components/inventory/modal-update-unit'
 import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
+import { Unit } from '@/lib/database/schema/inventory'
+import { Row, Table } from '@tanstack/react-table'
 import { useState } from 'react'
 import { TableActionsWrapper } from '../table/table-actions-wrapper'
-import { Row, Table } from '@tanstack/react-table'
-import { Unit } from '@/lib/database/schema/inventory'
-import { toggleBarredUnitAction } from '@/app/(site)/sys/enheder/actions'
-import { UpdateUnitForm } from '@/components/inventory/modal-update-unit'
 
+interface Props {
+  table: Table<Unit>
+  row: Row<Unit>
+}
 
+export function TableOverviewActions({ table, row }: Props) {
+  const [open, setOpen] = useState<boolean>(false)
+  const handleToggleBar = async () => {
+    const isCurrentlyBarred = row.original.isBarred
+    const updatedBarredStatus = !isCurrentlyBarred
+    const result = await toggleBarredUnitAction(
+      row.original.id,
+      updatedBarredStatus,
+    )
+  }
 
+  return (
+    <>
+      <TableActionsWrapper>
+        <DropdownMenuItem onClick={() => setOpen(true)}>
+          Rediger enhed
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={handleToggleBar}>
+          {row.original.isBarred ? 'Ophæv spærring' : 'Spær'}
+        </DropdownMenuItem>
+      </TableActionsWrapper>
 
- interface Props {
-    table: Table<Unit>
-    row: Row<Unit>
-    } 
-
-    export function TableOverviewActions({ table, row}: Props) {
-      const [open, setOpen] = useState<boolean>(false)
-      const handleToggleBar = async () => {
-        const isCurrentlyBarred = row.original.isBarred
-        const updatedBarredStatus = !isCurrentlyBarred
-        const result = await toggleBarredUnitAction(
-          row.original.id,
-          updatedBarredStatus,
-        )
-      }
-      
-    return (
-        <>
-          <TableActionsWrapper>
-            <DropdownMenuItem onClick={() => setOpen(true)}>
-              Redigér produkt
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={handleToggleBar}>
-              {row.original.isBarred ? 'Ophæv spærring' : 'Spær'}
-            </DropdownMenuItem>
-          </TableActionsWrapper>
-    
-      <UpdateUnitForm
-      unitToEdit={row.original}
-      isOpen={open}                     
-      setOpen={setOpen}
-      /> 
-      </>
-      )
-    }
+      <ModalUpdateUnit
+        unitToEdit={row.original}
+        isOpen={open}
+        setOpen={setOpen}
+      />
+    </>
+  )
+}
