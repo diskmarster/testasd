521d984e84cb9f8dc952a29b23290c5ca0c2b8b4
anvi <andreasgylche@gmail.com>
2024-09-28 11:06:18 +0200

    call signOutAction instead of redirecting

--- src/app/(site)/sys/enheder/page.tsx
+++ src/app/(site)/sys/enheder/page.tsx
@@ -1,3 +1,4 @@
+import { signOutAction } from '@/app/(auth)/log-ud/actions'
 import { SiteWrapper } from '@/components/common/site-wrapper'
 import { ModalCreateUnit } from '@/components/inventory/modal-create-unit'
 import { UnitOverview } from '@/components/inventory/table-units'
@@ -5,15 +6,16 @@ import { customerService } from '@/service/customer'
 import { inventoryService } from '@/service/inventory'
 import { locationService } from '@/service/location'
 import { sessionService } from '@/service/session'
-import { redirect } from 'next/navigation'
+
 export default async function Page() {
   const { session, user } = await sessionService.validate()
-  if (!session) redirect('/log-ind')
+  if (!session) return signOutAction()
   const location = await locationService.getLastVisited(user.id!)
-  if (!location) redirect('/log-ind')
+  if (!location) return signOutAction()
   const customer = await customerService.getByID(user.customerID)
-  if (!customer) redirect('/log-ind')
+  if (!customer) return signOutAction()
   const units = await inventoryService.getAllUnits()
+
   return (
     <SiteWrapper
       title='Enheder'
