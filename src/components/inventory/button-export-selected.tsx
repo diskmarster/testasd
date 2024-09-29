1aa292eb95c3db79cd91432a3ebf1e3c49cfafd4
anvi <andreasgylche@gmail.com>
2024-09-29 16:25:41 +0200

    remove tooltip

--- src/components/inventory/button-export-selected.tsx
+++ src/components/inventory/button-export-selected.tsx
@@ -1,11 +1,5 @@
 import { Button } from '@/components/ui/button'
 import { Icons } from '@/components/ui/icons'
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '@/components/ui/tooltip'
 import { exportTableToCSV } from '@/lib/export/csv'
 import { Table } from '@tanstack/react-table'
 import { useTransition } from 'react'
@@ -17,33 +11,24 @@ export function ExportSelectedButton<TData>({
 }) {
   const [pending, startTransition] = useTransition()
   return (
-    <TooltipProvider>
-      <Tooltip delayDuration={250}>
-        <TooltipTrigger asChild>
-          <Button
-            variant='outline'
-            size='icon'
-            className='bg-popover'
-            disabled={pending}
-            onClick={() => {
-              startTransition(() => {
-                exportTableToCSV(table, {
-                  excludeColumns: ['select', 'actions'],
-                  onlySelected: true,
-                })
-              })
-            }}>
-            {pending ? (
-              <Icons.spinner className='size-4 animate-spin' />
-            ) : (
-              <Icons.download className='size-4' />
-            )}
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent align='center'>
-          <p>Eksporter data</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <Button
+      variant='outline'
+      size='icon'
+      className='bg-popover'
+      disabled={pending}
+      onClick={() => {
+        startTransition(() => {
+          exportTableToCSV(table, {
+            excludeColumns: ['select', 'actions'],
+            onlySelected: true,
+          })
+        })
+      }}>
+      {pending ? (
+        <Icons.spinner className='size-4 animate-spin' />
+      ) : (
+        <Icons.download className='size-4' />
+      )}
+    </Button>
   )
 }
