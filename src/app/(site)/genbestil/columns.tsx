b3441e5d22a9418bb04a3a9430a01bb67e3c2f38
Lujhna <jakob_h_95@hotmail.com>
2024-09-30 10:28:32 +0200

    added limitation to only show 2 decimal numbers on both anbefalet genbestilt and bestilt

--- src/app/(site)/genbestil/columns.tsx
+++ src/app/(site)/genbestil/columns.tsx
@@ -130,7 +130,7 @@ export function getTableReorderColumns(
     header: ({ column }) => (
       <TableHeader column={column} title='Anbefalet genbestil' />
     ),
-    cell: ({ getValue }) => getValue<string>(),
+    cell: ({ getValue }) => getValue<number>().toFixed(2),
     filterFn: 'weakEquals',
     meta: {
       viewLabel: 'Anbefalet genbestil',
@@ -156,7 +156,7 @@ export function getTableReorderColumns(
   const orderedCol: ColumnDef<FormattedReorder> = {
     accessorKey: 'ordered',
     header: ({ column }) => <TableHeader column={column} title='Bestilt' />,
-    cell: ({ getValue }) => getValue<number>(),
+    cell: ({ getValue }) => getValue<number>().toFixed(2),
     filterFn: 'weakEquals',
     meta: {
       viewLabel: 'Bestilt',
