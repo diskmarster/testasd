2ec11ae9f500ef66570d987552201644208c2ebc
anvidev <andreasgylche@gmail.com>
2024-10-01 11:36:47 +0200

    protecting admin routes with a layout file

---
+++ src/components/common/layout-admin.tsx
@@ -0,0 +1,14 @@
+import { signOutAction } from '@/app/(auth)/log-ud/actions'
+import { sessionService } from '@/service/session'
+import { redirect } from 'next/navigation'
+
+export default async function LayoutAdmin({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  const { session, user } = await sessionService.validate()
+  if (!session) return signOutAction()
+  if (user.role == 'bruger') return redirect('/oversigt')
+  return <>{children}</>
+}
