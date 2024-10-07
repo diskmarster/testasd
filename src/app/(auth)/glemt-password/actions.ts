c671cda3b0619e3b8975f6585f7955d1f450d09d
Tobias <tobias@skancode.dk>
2024-10-07 14:40:19 +0200

    add resetPasswordAction

--- src/app/(auth)/glemt-password/actions.ts
+++ src/app/(auth)/glemt-password/actions.ts
@@ -1,9 +1,10 @@
 'use server'
 
 import { publicAction } from '@/lib/safe-action'
-import { ACTION_ERR_INTERNAL, ActionError } from '@/lib/safe-action/error'
+import { ACTION_ERR_INTERNAL, ACTION_ERR_UNAUTHORIZED, ActionError } from '@/lib/safe-action/error'
 import { passwordResetService } from '@/service/password-reset'
-import { forgotPasswordValidation } from './validation'
+import { forgotPasswordValidation, resetPasswordValidation } from './validation'
+import { isBefore } from 'date-fns'
 
 export const forgotPasswordAction = publicAction
 	.schema(forgotPasswordValidation)
@@ -12,6 +13,23 @@ export const forgotPasswordAction = publicAction
 		if (!linkCreated) {
 			throw new ActionError(
 				`${ACTION_ERR_INTERNAL}. Kunne ikke oprette nulstillings link`,
+			)
+		}
+	})
+
+export const resetPasswordAction = publicAction
+	.schema(resetPasswordValidation)
+	.action(async ({ parsedInput: { link, password } }) => {
+		if (isBefore(link.expiresAt, Date.now())) {
+			throw new ActionError(
+				`${ACTION_ERR_UNAUTHORIZED}. Link er udløbet`,
+			)
+		}
+
+		const pwResat = await passwordResetService.reset(link.userId, password)
+		if (!pwResat) {
+			throw new ActionError(
+				`${ACTION_ERR_INTERNAL}. Kunne ikke nulstille kodeord`,
 			)
 		}
 	})
