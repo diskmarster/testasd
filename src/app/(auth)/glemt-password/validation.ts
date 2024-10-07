b091df8f19c05da9292f5db2829246eb9b977c5a
Tobias <tobias@skancode.dk>
2024-10-07 14:38:12 +0200

    add resetPasswordValidation

--- src/app/(auth)/glemt-password/validation.ts
+++ src/app/(auth)/glemt-password/validation.ts
@@ -1,5 +1,29 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 export const forgotPasswordValidation = z.object({
 	email: z.string().email(),
 })
+
+export const resetPasswordValidation = z
+	.object({
+		link: z.object({
+			id: z.string(),
+			userId: z.number(),
+			expiresAt: z.number(),
+		}),
+		password: z
+			.string()
+			.min(8, { message: 'Kodeord skal minimum være 8 karakterer' }),
+		confirmPassword: z
+			.string()
+			.min(8, { message: 'Kodeord skal minimum være 8 karakterer' }),
+	})
+	.superRefine((val, ctx) => {
+		if (val.password != val.confirmPassword) {
+			ctx.addIssue({
+				path: ['password'],
+				code: z.ZodIssueCode.custom,
+				message: 'Kodeord er ikke ens.',
+			})
+		}
+	})
