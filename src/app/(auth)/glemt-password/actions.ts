243a075d5a2c439d3ec93ffecbbee3d5bc199fca
Tobias <tobias@skancode.dk>
2024-10-07 13:12:19 +0200

    check return from passwordResetService in forgotPasswordAction

--- src/app/(auth)/glemt-password/actions.ts
+++ src/app/(auth)/glemt-password/actions.ts
@@ -1,11 +1,17 @@
 'use server'
 
 import { publicAction } from '@/lib/safe-action'
+import { ACTION_ERR_INTERNAL, ActionError } from '@/lib/safe-action/error'
 import { passwordResetService } from '@/service/password-reset'
 import { forgotPasswordValidation } from './validation'
 
 export const forgotPasswordAction = publicAction
 	.schema(forgotPasswordValidation)
 	.action(async ({ parsedInput: { email } }) => {
-		await passwordResetService.createAndSendLink(email)
+		const linkCreated = await passwordResetService.createAndSendLink(email)
+		if (!linkCreated) {
+			throw new ActionError(
+				`${ACTION_ERR_INTERNAL}. Kunne ikke oprette nulstillings link`,
+			)
+		}
 	})
