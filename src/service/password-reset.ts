5890e649444bdbab0a435251302d44c35ced568f
Tobias <tobias@skancode.dk>
2024-10-07 11:48:46 +0200

    add createAndSendLink function to passwordResetService

--- src/service/password-reset.ts
+++ src/service/password-reset.ts
@@ -2,23 +2,57 @@ import { passwordReset } from '@/data/password-reset'
 import { ResetPasswordID } from '@/lib/database/schema/auth'
 import { ACTION_ERR_INTERNAL, ActionError } from '@/lib/safe-action/error'
 import { userService } from './user'
+import { emailService } from './email'
+import { EmailTest } from '@/components/email/email-test'
+
+const RESET_PASSWORD_LINK_BASEURL =
+  process.env.VERCEL_ENV === 'production'
+    ? 'https://lager.nemunivers.app'
+    : process.env.VERCEL_ENV === 'preview'
+      ? 'stage.lager.nemunivers.app'
+      : 'http://localhost:3000'
+
+export type ResetPasswordLink =
+  `${typeof RESET_PASSWORD_LINK_BASEURL}/glemt-password/${ResetPasswordID}`
+
+const LINK_DURATION_MINUTES = 30
 
 export const passwordResetService = {
-  create: async function(
+  createLink: async function(
     userEmail: string,
-  ): Promise<ResetPasswordID | undefined> {
+  ): Promise<ResetPasswordLink | undefined> {
     const user = await userService.getByEmail(userEmail)
     if (!user) {
       throw new ActionError('Ingen bruger med denne email fundet')
     }
 
     try {
-      return passwordReset.createPasswordReset(user.id)
+      const id: ResetPasswordID | undefined =
+        await passwordReset.createPasswordReset(user.id, LINK_DURATION_MINUTES)
+      if (!id) {
+        return undefined
+      }
+
+      return `${RESET_PASSWORD_LINK_BASEURL}/glemt-password/${id}`
     } catch (e) {
       console.error(e)
       throw new ActionError(
         `${ACTION_ERR_INTERNAL}. Kunne ikke oprette link til nulstilling af kodeord`,
       )
     }
+  },
+  createAndSendLink: async function(userEmail: string): Promise<boolean> {
+    const resetPasswordLink = this.createLink(userEmail)
+    if (!resetPasswordLink) {
+      return false
+    }
+
+    emailService.sendRecursively(
+      [userEmail],
+      "Nulstil kodeord",
+      EmailTest()
+    )
+
+    return true
   },
 }
