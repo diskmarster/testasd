8058f526ce6f9a54db5bac23fe2d55cced984409
Tobias <tobias@skancode.dk>
2024-10-07 08:58:39 +0200

    add resetPassword table to db

---
+++ src/lib/database/migrations/0004_broad_gressill.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `nl_reset_password` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` integer NOT NULL,
+	`expires_at` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `nl_user`(`id`) ON UPDATE no action ON DELETE cascade
+);
