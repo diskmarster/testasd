b76a32ef6247b6be9306773408b85bc29de490c3
Tobias <tobias@skancode.dk>
2024-10-07 13:20:05 +0200

    add resetPassword email

---
+++ src/components/email/email-reset-password.tsx
@@ -0,0 +1,42 @@
+import { siteConfig } from '@/config/site'
+import { CustomerActivationLink } from '@/service/customer'
+import { Heading, Link, Section, Tailwind, Text } from '@react-email/components'
+
+export function EmailResetPassword({
+  link,
+}: {
+  link: CustomerActivationLink
+}) {
+  return (
+    <Tailwind>
+      <Section>
+        <Heading as='h1'>Glemt kodeord!</Heading>
+        <Text>
+          Vi har modtaget en forespørgsel på at nulstille dit kodeord på{' '}
+          {siteConfig.name}
+        </Text>
+      </Section>
+      <Section>
+        <Text>
+          <Link href={link} target='_blank'>
+            Følg dette link
+          </Link>{' '}
+          for at nulstille dit kodeord.
+        </Text>
+      </Section>
+      <Section>
+        <Text>
+          Er det ikke dig som har forspugt om en nulstilling, så kan du ignorere
+          denne mail.
+        </Text>
+      </Section>
+      <Section>
+        <Text>
+          Venlig hilsen
+          <br />
+          SkanCode Teamet
+        </Text>
+      </Section>
+    </Tailwind>
+  )
+}
