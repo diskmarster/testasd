66a9fda556580c66ad44e01a38168dfb40e809e1
Tobias <tobias@skancode.dk>
2024-10-07 13:14:06 +0200

    increase next maxDuration for forgotPassword page

--- src/app/(auth)/glemt-password/page.tsx
+++ src/app/(auth)/glemt-password/page.tsx
@@ -1,5 +1,7 @@
 import { ForgotPasswordCard } from "@/components/auth/forgot-password";
 
+export const maxDuration = 60
+
 export default async function Page() {
 	return (
 		<section className="w-full">
