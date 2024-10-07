bd719bda2fba1316cbb225a965d1079ff7b2d4a3
Tobias <tobias@skancode.dk>
2024-10-07 12:22:47 +0200

    use forgotPasswordAction in forgotpassword page

--- src/components/auth/forgot-password.tsx
+++ src/components/auth/forgot-password.tsx
@@ -1,12 +1,14 @@
 'use client'
 
+import { forgotPasswordAction } from '@/app/(auth)/glemt-password/actions'
 import { forgotPasswordValidation } from '@/app/(auth)/glemt-password/validation'
 import { cn } from '@/lib/utils'
 import { zodResolver } from '@hookform/resolvers/zod'
 import Link from 'next/link'
-import { useTransition } from 'react'
+import { useState, useTransition } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { Alert, AlertDescription, AlertTitle } from '../ui/alert'
 import { Button, buttonVariants } from '../ui/button'
 import {
   Card,
@@ -21,6 +23,29 @@ import { Input } from '../ui/input'
 import { Label } from '../ui/label'
 
 export function ForgotPasswordCard() {
+  const [emailSent, setEmailSent] = useState<boolean>(false)
+  if (emailSent) {
+    return (
+      <div className='mx-auto max-w-lg space-y-4 text-center'>
+        <Icons.mail className='mx-auto h-12 w-12 animate-bounce text-primary' />
+        <h1 className='text-2xl font-bold tracking-tight text-foreground'>
+          Vi har modtaget din forespørgsel
+        </h1>
+        <p className='text-md text-foreground'>
+          Vi har sendt en mail til din e-mailadresse, som viser dig hvordan du kan nulstille dit kodeord. Tjek venligst
+          din indbakke, for at fortsætte.
+        </p>
+        <p className='text-sm text-muted-foreground'>
+          Hvis du ikke kan se e-mailen, tjek venligst din spam-mappe.
+        </p>
+        <Button asChild className='w-full'>
+          <Link href='/log-ind'>Gå til log ind siden</Link>
+        </Button>
+      </div>
+    )
+  }
+
+
   return (
     <Card className='relative w-full max-w-sm mx-auto'>
       <CardHeader>
@@ -30,7 +55,7 @@ export function ForgotPasswordCard() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <ForgotPasswordForm />
+        <ForgotPasswordForm setEmailSent={setEmailSent} />
       </CardContent>
       <CardFooter>
         <Link
@@ -46,8 +71,9 @@ export function ForgotPasswordCard() {
   )
 }
 
-function ForgotPasswordForm() {
+function ForgotPasswordForm({setEmailSent}: {setEmailSent: (val: boolean) => void}) {
   const [pending, startTransition] = useTransition()
+  const [error, setError] = useState<string>()
 
   const { register, formState, handleSubmit } = useForm<
     z.infer<typeof forgotPasswordValidation>
@@ -57,12 +83,26 @@ function ForgotPasswordForm() {
 
   const submitHandler = (values: z.infer<typeof forgotPasswordValidation>) => {
     startTransition(async () => {
-      console.log("reset password", values)
+      const res = await forgotPasswordAction(values)
+      if (res && res.serverError) {
+        setError(res.serverError)
+      } else {
+        setEmailSent(true)
+      }
     })
   }
 
   return (
-    <form className='grid w-full items-start gap-8' onSubmit={handleSubmit(submitHandler)}>
+    <form
+      className='grid w-full items-start gap-8'
+      onSubmit={handleSubmit(submitHandler)}>
+      {error && (
+        <Alert variant='destructive'>
+          <Icons.alert className='size-4 !top-3' />
+          <AlertTitle>Der skete en fejl</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      )}
       <div className='grid gap-2'>
         <Label htmlFor='email'>Email</Label>
         <Input id='email' type='email' {...register('email')} />
