bb20f57e89160cfaf3f490715b3fd9752b4c5132
Tobias <tobias@skancode.dk>
2024-10-07 09:46:01 +0200

    add password-reset data layer

---
+++ src/data/password-reset.ts
@@ -0,0 +1,29 @@
+import * as dateFns from "date-fns";
+import { db, TRX } from '@/lib/database'
+import {
+  ResetPasswordID,
+  resetPasswordTable,
+  UserID,
+} from '@/lib/database/schema/auth'
+import { generateId } from "lucia";
+
+export const passwordReset = {
+  createPasswordReset: async function(
+    userID: UserID,
+    trx: TRX = db,
+  ): Promise<ResetPasswordID | undefined> {
+    const id = generateId(16)
+    const expiry = dateFns.addMinutes(Date.now(), 30).getTime()
+
+    const [res] = await trx
+      .insert(resetPasswordTable)
+      .values({
+        id: id,
+        userId: userID,
+        expiresAt: expiry,
+      })
+      .returning({ id: resetPasswordTable.id })
+
+    return res?.id
+  },
+}
