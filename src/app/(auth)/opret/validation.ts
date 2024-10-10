b69023d18894cb3f121f03f492d6d0bf149ab1fb
Tobias <tobias@skancode.dk>
2024-10-10 14:03:42 +0200

    add extraUsers to signUpValidation

--- src/app/(auth)/opret/validation.ts
+++ src/app/(auth)/opret/validation.ts
@@ -4,5 +4,6 @@ import { z } from "zod";
 export const createCustomerValidation = z.object({
   company: z.string().min(2, { message: "Firmanavn skal minimum være 2 karakterer" }),
   email: z.string().email({ message: 'Email skal være gyldig' }),
-  plan: planZodSchema
+  plan: planZodSchema,
+  extraUsers: z.coerce.number().min(0).default(0),
 })
