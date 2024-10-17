9f4413cac1d758303cb53cb83bf15fdf9e61d647
anvidev <andreasgylche@gmail.com>
2024-10-17 08:30:13 +0200

    dropped migration for changing product unique constraints to prepare for rabase

--- src/lib/database/migrations/0006_damp_owl.sql
+++
@@ -1,3 +0,0 @@
-DROP INDEX IF EXISTS `nl_product_customer_id_sku_unique`;--> statement-breakpoint
-DROP INDEX IF EXISTS `nl_product_customer_id_barcode_unique`;--> statement-breakpoint
-CREATE UNIQUE INDEX `nl_product_customer_id_barcode_sku_unique` ON `nl_product` (`customer_id`,`barcode`,`sku`);
