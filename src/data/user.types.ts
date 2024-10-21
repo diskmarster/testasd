fae42557122a5252ca29fa57f18aad8511062939
anvidev <andreasgylche@gmail.com>
2024-10-21 10:08:31 +0200

    added new user roles to role type

--- src/data/user.types.ts
+++ src/data/user.types.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
 
-export const userRoleZodSchema = z.enum(['sys_admin', 'firma_admin', 'lokal_admin', 'bruger']);
+export const userRoleZodSchema = z.enum(['sys_admin', 'firma_admin', 'lokal_admin', 'bruger', 'afgang', 'læseadgang']);
 export type UserRole = z.infer<typeof userRoleZodSchema>;
 export const userRoles = userRoleZodSchema.options as readonly UserRole[];
