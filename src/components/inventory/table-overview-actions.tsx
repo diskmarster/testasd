a0360e72c3eb39720527b00e118bd99db9ae6a08
anvidev <andreasgylche@gmail.com>
2024-10-21 09:21:57 +0200

    hide "flyt beholdning" row action if plan is lite and add more data to event

--- src/components/inventory/table-overview-actions.tsx
+++ src/components/inventory/table-overview-actions.tsx
@@ -5,13 +5,15 @@ import { FormattedInventory } from '@/data/inventory.types'
 import { Row, Table } from '@tanstack/react-table'
 import { emitCustomEvent } from 'react-custom-events'
 import { TableActionsWrapper } from '../table/table-actions-wrapper'
+import { Plan } from '@/data/customer.types'
 
 interface Props {
   table: Table<FormattedInventory>
   row: Row<FormattedInventory>
+  plan: Plan
 }
 
-export function TableOverviewActions({ table, row }: Props) {
+export function TableOverviewActions({ table, row, plan }: Props) {
   return (
     <TableActionsWrapper>
       <DropdownMenuItem
@@ -20,22 +22,28 @@ export function TableOverviewActions({ table, row }: Props) {
             productName: row.original.product.text1,
             productID: row.original.product.id,
             placementID: row.original.placement.id,
+            placementName: row.original.placement.name,
             batchID: row.original.batch.id,
+            batchName: row.original.batch.batch
           })
         }}>
         Opdater beholdning
       </DropdownMenuItem>
-      <DropdownMenuItem
-        onClick={() => {
-          emitCustomEvent('MoveInventoryByIDs', {
-            productName: row.original.product.text1,
-            productID: row.original.product.id,
-            placementID: row.original.placement.id,
-            batchID: row.original.batch.id,
-          })
-        }}>
-        Flyt beholdning
-      </DropdownMenuItem>
+      {plan != 'lite' && (
+        <DropdownMenuItem
+          onClick={() => {
+            emitCustomEvent('MoveInventoryByIDs', {
+              productName: row.original.product.text1,
+              productID: row.original.product.id,
+              placementID: row.original.placement.id,
+              placementName: row.original.placement.name,
+              batchID: row.original.batch.id,
+              batchName: row.original.batch.batch
+            })
+          }}>
+          Flyt beholdning
+        </DropdownMenuItem>
+      )}
     </TableActionsWrapper>
   )
 }
