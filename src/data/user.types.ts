ffaebb30dbc94c349e1e96a83c3c4a9d37d25b13
anvidev <andreasgylche@gmail.com>
2024-10-21 13:39:09 +0200

    change roles to a clearer selection

--- src/data/user.types.ts
+++ src/data/user.types.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
 
-export const userRoleZodSchema = z.enum(['sys_admin', 'firma_admin', 'lokal_admin', 'bruger', 'afgang', 'læseadgang']);
+export const userRoleZodSchema = z.enum(['system_administrator', 'admininstrator', 'moderator', 'bruger', 'afgang', 'læseadgang']);
 export type UserRole = z.infer<typeof userRoleZodSchema>;
 export const userRoles = userRoleZodSchema.options as readonly UserRole[];
