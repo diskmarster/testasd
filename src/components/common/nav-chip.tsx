68f3542aee673050c9d96839310814fafca6fb85
anvidev <andreasgylche@gmail.com>
2024-10-25 10:31:29 +0200

    chip component

--- src/components/common/nav-chip.tsx
+++ src/components/common/nav-chip.tsx
@@ -2,20 +2,26 @@
 
 import { useEffect, useState } from "react"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip"
-import { getChipCount } from "@/lib/utils"
 import { useLanguage } from "@/context/language"
 import { useTranslation } from "@/app/i18n/client"
+import { useCustomEventListener } from "react-custom-events"
+import { getChipCount } from "@/lib/utils"
 
 export function NavChip({ chipLabel, localeKey }: { chipLabel: string, localeKey: string }) {
   const [count, setCount] = useState<number>()
   const lng = useLanguage()
   const { t } = useTranslation(lng, 'common')
 
+  useCustomEventListener("UpdateNavBadges", () => {
+    setCount(0)
+    getChipCount(chipLabel).then(c => setCount(c))
+  })
+
   useEffect(() => {
     if (!count) {
       getChipCount(chipLabel).then(c => setCount(c))
     }
-  }, [count, chipLabel])
+  }, [])
 
   if (!count || count == 0) return null
 
@@ -23,7 +29,7 @@ export function NavChip({ chipLabel, localeKey }: { chipLabel: string, localeKey
     <TooltipProvider>
       <Tooltip>
         <TooltipTrigger asChild>
-          <span className='bg-destructive rounded text-xs py-0.5 px-1 text-destructive-foreground font-semibold'>{count}</span>
+          <span className='bg-destructive tabular-nums rounded text-xs py-0.5 px-1 text-destructive-foreground font-semibold'>{count}</span>
         </TooltipTrigger>
         <TooltipContent className="bg-foreground text-background">
           {t(localeKey, { context: 'count', count })}
