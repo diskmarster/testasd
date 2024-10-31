af06bce35e2c659253d92229be0756db89628fa8
Lujhna <jakob_h_95@hotmail.com>
2024-10-31 10:04:14 +0100

    log-in validation translation

--- src/app/[lng]/(auth)/log-ind/validation.ts
+++ src/app/[lng]/(auth)/log-ind/validation.ts
@@ -3,5 +3,5 @@ import { z } from 'zod'
 export const signInValidation = (t: (key: string, options?: any) => string) =>
   z.object({
     email: z.string().email({ message: t('log-in.email') }),
-    password: z.string(),
+    password: z.string().min(8, { message: t('log-in.password') }),
   })
