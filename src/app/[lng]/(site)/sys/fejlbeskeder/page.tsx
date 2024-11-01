775425972823ee07730bdeb1affd289083de579b
anvidev <andreasgylche@gmail.com>
2024-11-01 10:06:52 +0100

    fix some translations for error msg page

--- src/app/[lng]/(site)/sys/fejlbeskeder/page.tsx
+++ src/app/[lng]/(site)/sys/fejlbeskeder/page.tsx
@@ -1,18 +1,22 @@
 import { signOutAction } from '@/app/[lng]/(auth)/log-ud/actions'
 import { SiteWrapper } from '@/components/common/site-wrapper'
 import { SkeletonTable } from '@/components/common/skeleton-table'
-import { TableErrors } from '@/components/errors/table-errors'
-import { Skeleton } from '@/components/ui/skeleton'
 import { hasPermissionByRank } from '@/data/user.types'
 import { customerService } from '@/service/customer'
-import { errorsService } from '@/service/errors'
 import { locationService } from '@/service/location'
 import { sessionService } from '@/service/session'
 import { redirect } from 'next/navigation'
 import { Suspense } from 'react'
 import { ServerTable } from './table'
+import { serverTranslation } from '@/app/i18n'
 
-export default async function Page() {
+interface Props {
+  params: {
+    lng: string
+  }
+}
+
+export default async function Page({ params }: Props) {
   const { session, user } = await sessionService.validate()
   if (!session) redirect('/login')
 
@@ -30,9 +34,10 @@ export default async function Page() {
     return
   }
 
+  const { t } = await serverTranslation(params.lng, 'errors')
 
   return (
-    <SiteWrapper title='Fejlbeskeder' description='Få et overblik over alle fejlbeskeder på NemLager'>
+    <SiteWrapper title={t('page.title')} description={t('page.description')}>
       <Suspense fallback={<SkeletonTable />}>
         <ServerTable />
       </Suspense>
