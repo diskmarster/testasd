33ca737d98866804d5b02167eaa9f52f5da3dc0d
anvidev <andreasgylche@gmail.com>
2024-11-07 11:12:02 +0100

    block server actions if maintenance is enabled

--- src/lib/safe-action/error.ts
+++ src/lib/safe-action/error.ts
@@ -2,4 +2,5 @@ export class ActionError extends Error {}
 
 export const ACTION_ERR_INTERNAL = 'Intern server fejl'
 export const ACTION_ERR_UNAUTHORIZED = 'Adgang nægtet'
+export const ACTION_ERR_MAINTENANCE = 'Vedligeholdelse i gang'
 export const ACTION_ERR_NOTFOUND = (resource: string) => `Kunne ikke finde ${resource}`
