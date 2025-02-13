61ab3b6b3741867a27e2ec960703459abfff24ee
anvidev <andreasgylche@gmail.com>
2025-02-13 13:00:50 +0100

    chore(config): add s3 file hostname to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nemlager-55t13hajf5.s3.eu-central-1.amazonaws.com',
+        port: '',
+        pathname: '/*/**',
+      },
+    ],
+  },
   experimental: {
-    serverComponentsExternalPackages: ["@node-rs/argon2"]
-  }
-};
+    serverComponentsExternalPackages: ['@node-rs/argon2'],
+    serverActions: {
+      bodySizeLimit: '4mb',
+    },
+  },
+}
 
-export default nextConfig;
+export default nextConfig
