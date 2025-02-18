3ff0cafdb64146f81e28b5030a7b5b96d1937313
anvidev <andreasgylche@gmail.com>
2025-02-18 14:13:26 +0100

    fix(types): make fileCount optional for compatability where functions use type

--- src/data/products.types.ts
+++ src/data/products.types.ts
@@ -3,7 +3,7 @@ import { Product } from '@/lib/database/schema/inventory'
 export interface FormattedProduct extends Product {
   unit: string
   group: string
-  fileCount: number
+  fileCount?: number
 }
 
 export const units = [
