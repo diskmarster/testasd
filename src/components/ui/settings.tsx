50d2ede39bd7e9122a1e7c086fc2a84eff527a98
Tobias <t.hc199811@gmail.com>
2025-02-21 15:07:05 +0100

    ui(settings): Introduce settings ui components

---
+++ src/components/ui/settings.tsx
@@ -0,0 +1,93 @@
+import { cn } from "@/lib/utils"
+import { HTMLProps } from "react"
+
+
+export function SettingWrapper({
+  className,
+  children,
+  ...props
+}: HTMLProps<HTMLDivElement>) {
+  return (
+    <div className={cn('flex flex-col', className)} {...props}>
+      {children}
+    </div>
+  )
+}
+
+export function SettingLabel({
+  className,
+  children,
+  ...props
+}: HTMLProps<HTMLDivElement>) {
+  return (
+    <div
+      className={cn('flex flex-col flex-1 justify-start p-4 pe-0', className)}
+      {...props}>
+      {children}
+    </div>
+  )
+}
+
+export function SettingTitle({
+  className,
+  children,
+  ...props
+}: HTMLProps<HTMLHeadingElement>) {
+  return (
+    <h3 className={cn('font-semibold tracking-tight', className)} {...props}>
+      {children}
+    </h3>
+  )
+}
+
+export function SettingDescription({
+  className,
+  children,
+  ...props
+}: HTMLProps<HTMLParagraphElement>) {
+  return (
+    <p
+      className={cn('text-xs text-muted-foreground text-pretty', className)}
+      {...props}>
+      {children}
+    </p>
+  )
+}
+
+export function SettingContent({
+  className,
+  children,
+  ...props
+}: HTMLProps<HTMLDivElement>) {
+  return (
+    <div className={cn('flex flex-1 justify-end p-4 ps-0', className)} {...props}>
+      {children}
+    </div>
+  )
+}
+
+export function SettingBody({
+  className,
+  children,
+  ...props
+}: HTMLProps<HTMLDivElement>) {
+  return (
+    <div
+      className={cn('flex items-center justify-between', className)}
+      {...props}>
+      {children}
+    </div>
+  )
+}
+
+export function SettingFooter({
+  className,
+  children,
+  ...props
+}: HTMLProps<HTMLDivElement>) {
+  return (
+    <div className={cn('flex p-4 pt-0', className)} {...props}>
+      {children}
+    </div>
+  )
+}
