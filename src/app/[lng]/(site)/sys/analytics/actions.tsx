29172e6d554de3df186e556be2e88866e8a78587
Tobias <t.hc199811@gmail.com>
2025-03-06 11:00:55 +0100

    fix(auth): make sys only available to sysadmin

--- src/app/[lng]/(site)/sys/analytics/actions.tsx
+++ src/app/[lng]/(site)/sys/analytics/actions.tsx
@@ -1,10 +1,10 @@
 'use server'
 
-import { adminAction } from '@/lib/safe-action'
+import { sysAdminAction } from '@/lib/safe-action'
 import { getActiveUsersAnalyticsValidation } from './validation'
 import { analyticsService } from '@/service/analytics'
 
-export const getActiveUsersAnalyticsAction = adminAction
+export const getActiveUsersAnalyticsAction = sysAdminAction
 	.schema(getActiveUsersAnalyticsValidation)
 	.action(async ({ parsedInput }) => {
 		const data = await analyticsService.getActiveUsers(parsedInput.start, parsedInput.end, parsedInput.groupBy)
--- src/app/[lng]/(site)/sys/analytics/page.tsx
+++ src/app/[lng]/(site)/sys/analytics/page.tsx
@@ -1,33 +1,15 @@
-import { signOutAction } from '@/app/[lng]/(auth)/log-ud/actions'
 import { ActiveUsersChartContainer } from '@/components/analytics/active-users-chart-container'
 import { SiteWrapper } from '@/components/common/site-wrapper'
-import { hasPermissionByRank } from '@/data/user.types'
-import { customerService } from '@/service/customer'
-import { locationService } from '@/service/location'
-import { sessionService } from '@/service/session'
-import { redirect } from 'next/navigation'
+import { withAuth, WithAuthProps } from '@/components/common/with-auth'
 
-export default async function Page() {
-  const { session, user } = await sessionService.validate()
-  if (!session) redirect('/login')
+interface Props extends WithAuthProps {}
 
-  if (!hasPermissionByRank(user.role, 'system_administrator')) {
-    redirect("/oversigt")
-  }
-  const location = await locationService.getLastVisited(user.id!)
-  if (!location) {
-    signOutAction()
-    return
-  }
-  const customer = await customerService.getByID(user.customerID)
-  if (!customer) {
-    signOutAction()
-    return
-  }
-
+async function Page(_: Props) {
   return (
     <SiteWrapper>
       <ActiveUsersChartContainer />
     </SiteWrapper>
   )
 }
+
+export default withAuth(Page, undefined, 'system_administrator')
