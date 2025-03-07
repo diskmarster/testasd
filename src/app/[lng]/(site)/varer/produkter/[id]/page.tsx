ecfa17773c68ccef370603acfb637e2c15cfe4ff
anvidev <andreasgylche@gmail.com>
2025-03-07 10:48:03 +0100

    fix(ui): use withAuth on product details page

--- src/app/[lng]/(site)/varer/produkter/[id]/page.tsx
+++ src/app/[lng]/(site)/varer/produkter/[id]/page.tsx
@@ -1,9 +1,5 @@
-
-import { signOutAction } from '@/app/[lng]/(auth)/log-ud/actions'
 import { SiteWrapper } from '@/components/common/site-wrapper'
-import { hasPermissionByPlan, hasPermissionByRank } from '@/data/user.types'
-import { sessionService } from '@/service/session'
-import { redirect } from 'next/navigation'
+import { hasPermissionByPlan } from '@/data/user.types'
 import { Suspense } from 'react'
 import { ProductDetailsWrapper } from './details-wrapper'
 import { ProductFilesWrapper } from './files-wrapper'
@@ -12,25 +8,16 @@ import { DetailsSkeleton } from '@/components/products/product-details'
 import { FilesSkeleton } from '@/components/products/product-files-grid'
 import { HistorySkeleton } from '@/components/products/product-history'
 import { ReorderWrapper } from './reorder-wrapper'
+import { withAuth, WithAuthProps } from '@/components/common/with-auth'
 
-interface PageProps {
+interface PageProps extends WithAuthProps {
 	params: {
 		lng: string
 		id: string
 	}
 }
 
-export default async function Page({ params: { lng, id } }: PageProps) {
-	const { session, user, customer } = await sessionService.validate()
-	if (!session) {
-		signOutAction()
-		return
-	}
-
-	if (!hasPermissionByRank(user.role, 'læseadgang')) {
-		redirect('/oversigt')
-	}
-
+async function Page({ params: { lng, id }, user, customer }: PageProps) {
 	return (
 		<SiteWrapper>
 			<div className='flex flex-col lg:flex-row items-stretch gap-4'>
@@ -55,4 +42,6 @@ export default async function Page({ params: { lng, id } }: PageProps) {
 			</div>
 		</SiteWrapper>
 	)
-}
+} 
+
+export default withAuth(Page, undefined, 'læseadgang')
