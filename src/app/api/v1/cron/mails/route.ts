0eac9a0198eb2d9a68e709c37333ee676b70a86d
anvidev <andreasgylche@gmail.com>
2025-03-27 11:09:41 +0100

    feat(api): added endpoint for cron job to fetch all mails (wip)

---
+++ src/app/api/v1/cron/mails/route.ts
@@ -0,0 +1,38 @@
+import { CustomerMailSettingWithEmail } from '@/data/customer.types'
+import { customerService } from '@/service/customer'
+import { NextRequest, NextResponse } from 'next/server'
+
+const CRON_SECRET = process.env.CRON_SECRET
+
+export async function GET(request: NextRequest) {
+  const secret = request.headers.get('Authorization')
+
+  if (!secret) {
+    return sendResponse(401, { error: 'authorization is missing' })
+  }
+
+  const parts = secret.split(' ', 2)
+  if (parts[0] != 'Bearer') {
+    return sendResponse(401, { error: 'authorization is malformed' })
+  }
+
+  if (parts[1] != CRON_SECRET) {
+    return sendResponse(401, { error: 'authorization is denied' })
+  }
+
+  let mails: CustomerMailSettingWithEmail[] = []
+
+  try {
+    mails = await customerService.getMailsForCron()
+  } catch (error) {
+    const errMsg =
+      error instanceof Error ? error.message : 'unknown error occured'
+    return sendResponse(500, { error: errMsg })
+  }
+
+  return sendResponse(200, { mails })
+}
+
+function sendResponse(code: number, data: any) {
+  return NextResponse.json(data, { status: code })
+}
