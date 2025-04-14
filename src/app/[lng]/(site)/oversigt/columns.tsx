ba740f8b1ff0f205576731b066935c1b7031514f
Tobias <t.hc199811@gmail.com>
2025-04-14 09:45:20 +0200

    fix(disp): also display disp qty on overview table if not grouped

--- src/app/[lng]/(site)/oversigt/columns.tsx
+++ src/app/[lng]/(site)/oversigt/columns.tsx
@@ -283,7 +283,6 @@ export function getTableOverviewColumns(
     ),
     aggregatedCell: ({row}) => {
 			const value = row.original.disposable
-			console.log(value)
 			return value != null ? (
 				<span className={cn(value < 0 && 'text-destructive')}>
 					{formatNumber(value, lng)}
@@ -293,7 +292,21 @@ export function getTableOverviewColumns(
 			)
 		},
 		aggregationFn: 'unique',
-    cell: () => null,
+		cell: ({ table, row }) => {
+			/*@ts-ignore*/
+			if (table.options.meta?.isGrouped) {
+				return null
+			}
+
+			const value = row.original.disposable
+			return value != null ? (
+				<span className={cn(value < 0 && 'text-destructive')}>
+					{formatNumber(value, lng)}
+				</span>
+			) : (
+				<span>-</span>
+			)
+		},
     filterFn: (row, id, value) => numberRangeFilterFn(row, id, value),
     meta: {
       rightAlign: true,
