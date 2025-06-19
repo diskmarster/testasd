dfd48b144bd160ce1f13c306af10357963617eb4
anvidev <andreasgylche@gmail.com>
2025-06-19 12:16:23 +0200

    feat: nw endpoint to fetch product information

---
+++ src/app/api/v2/public/products/[id]/route.ts
@@ -0,0 +1,68 @@
+import { serverTranslation } from '@/app/i18n'
+import { FormattedProduct } from '@/data/products.types'
+import { getVercelRequestID, validatePublicRequest } from '@/lib/api/request'
+import { apiResponse, ApiResponse } from '@/lib/api/response'
+import { Inventory } from '@/lib/database/schema/inventory'
+import { tryParseInt } from '@/lib/utils'
+import { isMaintenanceMode, tryCatch } from '@/lib/utils.server'
+import { productService } from '@/service/products'
+import { getLanguageFromRequest } from '@/service/user.utils'
+import { headers } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(
+  r: NextRequest,
+  { params }: { params: { id: string } },
+): Promise<
+  NextResponse<
+    ApiResponse<
+      | (FormattedProduct & {
+          inventories: Inventory[]
+        })
+      | undefined
+    >
+  >
+> {
+  const lng = getLanguageFromRequest(headers())
+  const { t } = await serverTranslation(lng, 'common')
+
+  if (isMaintenanceMode()) {
+    return apiResponse.locked(
+      t('route-translations-regulations.maintenance'),
+      getVercelRequestID(headers()),
+    )
+  }
+
+  const { customer } = await validatePublicRequest(headers())
+  if (customer == null) {
+    return apiResponse.unauthorized(
+      t('route-translations-product.no-access-to-resource'),
+      getVercelRequestID(headers()),
+    )
+  }
+
+  const id = tryParseInt(params.id)
+  if (!id) {
+    return apiResponse.badRequest(
+      t('route-translations-productid.error-product-invalid-id'),
+      getVercelRequestID(headers()),
+    )
+  }
+
+  const productRes = await tryCatch(productService.getByID(id))
+  if (!productRes.success) {
+    return apiResponse.internal(
+      productRes.error.message,
+      getVercelRequestID(headers()),
+    )
+  }
+
+  if (!productRes.data) {
+    return apiResponse.notFound(
+      t('route-translations-productid.error-product-notfound'),
+      getVercelRequestID(headers()),
+    )
+  }
+
+  return apiResponse.ok(productRes.data)
+}
