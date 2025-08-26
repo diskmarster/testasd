8131c41bca9ed1a3ce8921b292b51dc6fc74dfb5
Map94 <Markp@live.dk>
2025-08-26 15:19:26 +0200

    feat: batch-filter-added-with-barred-shown

--- src/app/[lng]/(site)/historik/columns.tsx
+++ src/app/[lng]/(site)/historik/columns.tsx
@@ -611,6 +611,9 @@ export function getTableHistoryFilters(
 			...batches.map(batch => ({
 				value: batch.batch,
 				label: batch.batch,
+				icon:  batch.isBarred 
+									? <span className={cn('size-2 rounded-full bg-destructive/50 border border-destructive cursor-pointer block')}/>
+									: undefined
 			})),
 		],
 	}
